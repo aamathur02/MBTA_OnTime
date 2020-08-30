f45e206554a1a03310eb78f9922153a0463c02cf
Aayush Mathur <aayushmathur@lawn-128-61-15-237.lawn.gatech.edu>
2020-08-30 14:35:16 -0400

    added ending stop funtionality

--- OnTime/urls.py
+++ OnTime/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('add/', views.TripCreateView.as_view(), name = 'trip_add'),
     path('<int:pk>/', views.TripUpdateView.as_view(),name = 'trip_change'),
 
-    path('ajax/load-stops/', views.load_stops, name='ajax_load_stops'),  # <-- this one here
-
+    path('ajax/load-starting_stops/', views.load_starting_stops, name='ajax_load_starting_stops'),  # <-- this one here
+    path('ajax/load-ending_stops/', views.load_ending_stops, name='ajax_load_ending_stops'),
 ]
--- OnTime/views.py
+++ OnTime/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Trip, Starting_Stop
+from .models import Trip, Starting_Stop, Ending_Stop
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
 from .forms import TripForm
@@ -22,10 +22,20 @@ class TripUpdateView(UpdateView):
     form_class  = TripForm
     success_url = reverse_lazy('trip_changelist')
 
-def load_stops(request):
-    print("ran yoooload_stops")
+def load_starting_stops(request):
+    print("ran starting stops")
 
     line_id = request.GET.get('line')
     starting_stops = Starting_Stop.objects.filter(line_id=line_id).order_by('name')
     print(starting_stops)
-    return render(request, 'OnTime/stop_dropdown_list.html', {'starting_stops':starting_stops}) 
+    return render(request, 'OnTime/starting_stop_dropdown_list.html', {'starting_stops':starting_stops})
+
+def load_ending_stops(request):
+    print("ran endinf stops")
+
+    line_id = request.GET.get('line')
+    ending_stops = Ending_Stop.objects.filter(line_id=line_id).order_by('name')
+    print(ending_stops)
+    return render(request, 'OnTime/ending_stop_dropdown_list.html', {'ending_stops':ending_stops}) 
+
+    
